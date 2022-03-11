6e0f37f99b4061eafe787900cc6e40781a6a0f13
Luis Antonio <antonhy.romero98@gmail.com>
2022-03-11 01:48:30 -0600

    Mejorado todo el GUI. Agregados hasta botones para reiniciar e instrucciones.

--- js/script.js
+++ js/script.js
@@ -1,48 +1,53 @@
-let playerOptions = document.querySelectorAll("button");
+let playerOptions = document.querySelectorAll("button.option-btn");
+let buttonNext = document.getElementById("next");
+let buttonReset = document.getElementById("reset");
+let buttonAbout = document.getElementById("about");
+let resultDiv = document.getElementById("roundResult");
+let matchDiv = document.getElementById("matchResult");
+
 let currentRound = 0, player1Wins = 0, player2Wins = 0, counter = 0;
 const MAXWINS = 5;
 
-playerOptions.forEach(button => {
-    button.addEventListener("click", showResult);
+initButtonsListeners();
+let process = window.setInterval(rotateMovs,200);
+buttonReset.addEventListener("click", resetGame);
+buttonAbout.addEventListener("click",() =>{
+    resultDiv.textContent = "The game is easy to play and to understand: Basically you (the Player) must try to beat his opponent (the Computer) by choosing between 3 differents options: ROCK, PAPER or SCISSORS.";
+    matchDiv.textContent = "ROCK beats SCISSORS, SCISSORS beat PAPER and PAPER beats ROCK. The computer will decide randomly between the same options. At the end the first player that reaches 5 wins is the complete WINNER of the game. Remember to look at here (the Console or Results) to see how the game is going. Good Luck and have FUN. ";
+});
+buttonNext.addEventListener("click", () =>{
+    buttonNext.classList.add("hidden");
+    initButtonsListeners();
+    process = window.setInterval(rotateMovs,200);
 });
 
-function showResult(event){
-    let resultDiv = document.getElementById("roundResult");
-    let matchDiv = document.getElementById("matchResult");
-    let option = event.currentTarget.querySelector(".option-img");
-    matchDiv.style.whiteSpace = "pre-wrap";
-
-    resultDiv.textContent = playRound(option.alt,computerPlay());
-    
-    if(player1Wins == MAXWINS || player2Wins == MAXWINS){
-        playerOptions.forEach(button => button.removeEventListener("click", showResult));
-        matchDiv.textContent = player1Wins > player2Wins ? "Player Wins! Hoora!" : "Computer Wins...Better Luck Next Time";
-    }else{
-        matchDiv.textContent = `Number of Rounds Played = ${currentRound} \nPlayer: ${player1Wins} \nComputer: ${player2Wins}`;
-    }
+function initButtonsListeners() {
+    playerOptions.forEach(button => {
+        button.addEventListener("click", showResult);
+    });
 }
 
-function nextMov() {
+function resetGame(){
+    currentRound = 0, player1Wins = 0, player2Wins = 0, counter = 0;
+    initButtonsListeners();
+    resultDiv.textContent = "Games has been restarted!";
+    matchDiv.textContent = "";
+    if(!process) process = window.setInterval(rotateMovs,200);
+}
+
+function rotateMovs() {
     let movs = ["rock", "paper", "scissors"];
     counter = (counter + 1) % movs.length;
 
     let currMov = document.getElementById("movComp");
-    let newMov = document.createElement("img");
-    newMov.id = "movComp";
-    newMov.src = `./img/${movs[counter]}.png`;
-    newMov.alt = movs[counter];
-    newMov.classList.add("option-img");
-
-    currMov.parentElement.replaceChild(newMov, currMov);
+    currMov.src = `./img/${movs[counter]}.png`;
+    currMov.alt = movs[counter];  
 }
 
-let process = window.setInterval(nextMov,200);
-
 function computerPlay(){
-    clearInterval(process);
+    process = clearInterval(process);
     let currMov = document.getElementById("movComp");
     let compPlay = currMov.alt;
-    process = window.setInterval(nextMov,200);
     return compPlay;
 }
 
@@ -51,28 +56,43 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
 
     if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        result = 'You Win! Rock beats Scissors';
+        result = 'You Win! Rock beats Scissors.';
         player1Wins++;
     }else if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        result = 'You Win! Scissors beats Paper';
+        result = 'You Win! Scissors beats Paper.';
         player1Wins++;
     }else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        result = 'You Win! Paper beats Rock';
+        result = 'You Win! Paper beats Rock.';
         player1Wins++;
     }else if(playerSelection === 'rock' && computerSelection === 'paper'){
-        result = 'You Lose! Paper beats Rock';
+        result = 'You Lose! Paper beats Rock.';
         player2Wins++;
     }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
-        result = 'You Lose! Rock beats Scissors';
+        result = 'You Lose! Rock beats Scissors.';
         player2Wins++;
     }else if(playerSelection === 'paper' && computerSelection === 'scissors'){
-        result = 'You Lose! Scissors beats Paper';
+        result = 'You Lose! Scissors beats Paper.';
         player2Wins++;
     }else if(playerSelection === computerSelection){
         result = `It\'s a Tie between ${playerSelection}!`;
     }else{
-        result = 'Error: User has written an invalid option!';
+        result = 'Error: Input Invalid.';
     }
     currentRound++;
     return result;
+}
+
+function showResult(event){
+    let option = event.currentTarget.querySelector(".option-img");
+    matchDiv.style.whiteSpace = "pre-wrap";
+
+    resultDiv.textContent = playRound(option.alt,computerPlay());
+    
+    if(player1Wins == MAXWINS || player2Wins == MAXWINS){
+        matchDiv.textContent = player1Wins > player2Wins ? "Player Wins! Hoora!" : "Computer Wins...Better Luck Next Time";
+    }else{
+        buttonNext.classList.remove("hidden");
+        matchDiv.textContent = `Number of Rounds Played: ${currentRound} \nNumber of Rounds Won by Player: ${player1Wins} \nNumber of Rounds Won by Computer: ${player2Wins}`;
+    }
+    playerOptions.forEach(button => button.removeEventListener("click", showResult));
 }
